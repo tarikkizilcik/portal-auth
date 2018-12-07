c67e708e104891dd98cda55bcfc712669cf8bf19
Nejdet <necsrky@gmail.com>
2018-12-07 10:33:43 +0300

    initial commit

---
+++ controllers/frontend/DefaultController.php
@@ -0,0 +1,18 @@
+<?php
+namespace kouosl\login\controllers\frontend;
+
+
+/**
+ * Default controller for the `login` module
+ */
+class DefaultController extends \kouosl\base\controllers\frontend\BaseController
+{
+    /**
+     * Renders the index view for the module
+     * @return string
+     */
+    public function actionIndex()
+    {
+        return $this->render('_index');
+    }
+}
