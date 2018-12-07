81b270cb02609e8e3db9205a37c5b84340e0d963
tarikkizilcik <trk.kzlck@hotmail.com>
2018-12-07 10:33:43 +0300

    initial commit

---
+++ controllers/api/DefaultController.php
@@ -0,0 +1,18 @@
+<?php
+namespace kouosl\login\controllers\api;
+
+
+/**
+ * Default controller for the `login` module
+ */
+class DefaultController extends \kouosl\base\controllers\api\BaseController
+{
+    /**
+     * Renders the index view for the module
+     * @return string
+     */
+    public function actionIndex()
+    {
+        return $this->render('_index');
+    }
+}
